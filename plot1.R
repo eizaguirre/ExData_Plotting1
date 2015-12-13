7bcf9621a5b8cee490fd48647d142be283366854
eizaguirre <erick.izaguirre@outlook.com>
2015-12-13 16:09:37 -0600

    Commit

---
+++ plot1.R
@@ -0,0 +1,14 @@
+#Reads source file and creates a new datetime column 
+ihepc <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors=FALSE)
+ihepc$Datetime <- paste(ihepc$Date, ihepc$Time)
+ihepc$Datetime <- strptime(ihepc$Datetime, "%d/%m/%Y %H:%M:%S")
+
+#Subsets the data to only include observations from 2007-02-01 and 2007-02-02
+ihepc <- subset(ihepc, ihepc$Datetime >= "2007-02-01 00:00:00" & ihepc$Datetime <= "2007-02-02 23:59:59" )
+
+#Creates a Histogram with the Global_active_power variable
+hist(as.numeric(ihepc$Global_active_power), col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+
+#Exports the Histogram to a PNG file
+dev.copy(png, file = "plot1.png")
+dev.off()
